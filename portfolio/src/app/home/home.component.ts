d1608f3da91a99b074cf563a76d81912fd6f2d9c
Walter TimothyJos Lopez <Walter.Lopez@everbank.com>
2021-03-20 02:25:40 -0400

    Added the last section to the home page. With styling

--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgStyle } from "@angular/common";
 
 @Component({
   selector: 'app-home',
